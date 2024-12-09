d320ab943a5e6283eb06eb7688744028c9183fb8
abdulhadi240 <ah912425@gmail.com>
2024-12-09 13:38:16 +0500

    users page done

--- app/customers/[customer]/page.jsx
+++ app/customers/[customer]/page.jsx
@@ -56,10 +56,11 @@ const router = useRouter();
     );
   
     if (!updatedData.statusCode) {
-      toast.success("Court updated successfully.");
+      toast.success("Customer updated successfully.");
       router.push("/customers");
+      
     } else {
-      toast.error("Failed to update the court.");
+      toast.error("Failed to update the Customer.");
     }
   };
 
@@ -72,7 +73,7 @@ const router = useRouter();
       <div className="w-full flex justify-center items-center">
         <div className="bg-white rounded-lg md:p-0 p-2 w-full max-w-6xl">
           <div className="flex items-center mb-4 gap-3">
-            <Link href="/grounds" className="h-8 w-8 flex justify-center items-center pl-1 rounded-full bg-[#f9f9fb] text-primary1">
+            <Link href="/customers" className="h-8 w-8 flex justify-center items-center pl-1 rounded-full bg-[#f9f9fb] text-primary1">
               <MdArrowBackIos size={15} />
             </Link>
             <h1 className="text-xl">
@@ -85,68 +86,68 @@ const router = useRouter();
           >
             {/* Left Column */}
             <div className="space-y-4">
-              <div className="flex justify-between">
-                <div className="w-1/2 pr-2">
-                  <label className="block text-sm">Name</label>
+              <div className="md:flex md:justify-between space-y-4 md:space-y-0">
+                <div className="md:w-1/2 md:pr-2">
+                  <label className="block md:text-sm text-xs">Name</label>
                   <input
                     type="text"
                     placeholder="Enter Name"
                     value={name}
                     onChange={(e) => setName(e.target.value)}
-                    className="w-56 mt-1 p-2 text-sm bg-[#f9f9fb] border-gray-300 rounded"
+                    className="w-full md:w-56 mt-1 p-2 text-sm bg-[#f9f9fb] border-gray-300 rounded"
                   />
                 </div>
-                <div className="w-1/2 pl-2">
-                  <label className="block text-sm">Email</label>
+                <div className="md:w-1/2 md:pr-2">
+                  <label className="block md:text-sm text-xs">Email</label>
                   <input
                     type="email"
                     placeholder="Enter Email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
-                    className="w-56 mt-1 p-2 text-sm bg-[#f9f9fb] border-gray-300 rounded"
+                    className="w-full md:w-56 mt-1 p-2 text-sm bg-[#f9f9fb] border-gray-300 rounded"
                   />
                 </div>
               </div>
-              <div className="flex justify-between">
-                <div className="w-1/2 pr-2">
-                  <label className="block text-sm">Phone Number</label>
+              <div className="md:flex md:justify-between space-y-4 md:space-y-0">
+                <div className="md:w-1/2 md:pr-2">
+                  <label className="block md:text-sm text-xs">Phone Number</label>
                   <input
                     type="text"
                     placeholder="Enter your phone number"
                     value={user_phone}
                     onChange={(e) => setUser_Phone(e.target.value)}
-                    className="w-56 mt-1 p-2 text-sm bg-[#f9f9fb] border-gray-300 rounded"
+                    className="w-full md:w-56 mt-1 p-2 text-sm bg-[#f9f9fb] border-gray-300 rounded"
                   />
                 </div>
-                <div className="w-1/2 pr-2">
-                  <label className="block text-sm">Second Phone Number</label>
+                <div className="md:w-1/2 md:pr-2">
+                  <label className="block md:text-sm text-xs">Second Phone Number</label>
                   <input
                     type="text"
                     placeholder="03120202881"
                     value={secondary_user_phone}
                     onChange={(e) => setSecondary_User_Phone(e.target.value)}
-                    className="w-56 mt-1 p-2 text-sm bg-[#f9f9fb] border-gray-300 rounded"
+                    className="w-full md:w-56 mt-1 p-2 text-sm bg-[#f9f9fb] border-gray-300 rounded"
                   />
                 </div>
               </div>
-              <label className="block text-sm">Password</label>
+              <label className="block md:text-sm text-xs">Password</label>
               <div className="relative">
                 <input
                   type={showPassword ? 'text' : 'password'}
                   placeholder="********"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
-                  className="w-full mt-1 p-2  pr-8 border bg-[#f9f9fb] text-sm border-gray-300 rounded"
+                  className="w-full -mt-3 p-2  pr-8 border bg-[#f9f9fb] text-sm border-gray-300 rounded"
                 />
                 <div
-                  className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer"
+                  className="absolute inset-y-0 right-0 pr-3 -mt-1 flex items-center cursor-pointer"
                   onClick={() => setShowPassword(!showPassword)}
                 >
                   {showPassword ? <FaEyeSlash /> : <FaEye />}
                 </div>
               </div>
             </div>
-            <div>
+            <div className="flex justify-center">
               <label className="bg-[#f9f9fb] font-semibold text-base rounded max-w-md h-56 w-52 flex flex-col items-center justify-center cursor-pointer border border-gray-300">
                 <div className="h-14 w-14 rounded-full bg-[#ece8f2] flex justify-center items-center">
                   <FaPlus size={20} />
@@ -157,7 +158,7 @@ const router = useRouter();
                 </p>
               </label>
             </div>
-            <div className="flex justify-start">
+            <div className="flex justify-center md:justify-start">
               <button
                 type="submit"
                 className="bg-primary1 text-white w-auto ml-2 px-10 p-2 rounded hover:bg-primary1/80"
